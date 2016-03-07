00db40734ad09fa3b50333209c56bc80a9953c82
rtuttleGit <Ryan@ryans-mbp.frontier.com>
2016-03-07 17:09:47 -0500

    Initial Commit

---
+++ MassRoots/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MassRoots
+//
+//  Created by Ryan on 3/7/16.
+//  Copyright © 2016 Code With Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MassRoots/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MassRoots
+//
+//  Created by Ryan on 3/7/16.
+//  Copyright © 2016 Code With Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
